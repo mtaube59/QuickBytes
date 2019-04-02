ef8c63cf96494ab803a37b5d0658ec51f9141942
SydPRetzel <myskandev@gmail.com>
2019-04-01 22:41:45 -0400

    Added modal for restaurant details.

--- assets/javascript/data_presenters.js
+++ assets/javascript/data_presenters.js
@@ -33,6 +33,53 @@ function updateTable() {
             insertRow(i);
         }
     }
+
+    $("#my-table-body tr").on("click", function (event) {
+
+        console.log("row clicked");
+        console.log($(this));
+        var id = parseInt($(this).attr("id"));
+        console.log("id = " + id);
+
+
+        // Set the info on the modal based on which result was clicked.
+        $("#modal-card-title").text(restaurantData.getPlaceName(id));
+        if (restaurantData.getPlaceCuisines(id) != "") {
+            $("#cuisine-text").text("Cuisine : " + restaurantData.getPlaceCuisines(id));
+        }
+        if (restaurantData.getPlaceRatingText(id) != "") {
+            $("#rating-text").text("Rating : " + restaurantData.getPlaceRatingText(id));
+        }
+        $("#price-text").text("Price : " + restaurantData.getPlacePriceRange(id));
+        // Menu link
+        var menu = restaurantData.getPlaceMenuURL(id);
+        if (menu != null && menu != "") {
+            $("#menu-text").text("MENU");
+            $("#menu-text").attr("href",menu);
+            
+        }
+
+
+        // Get driving directions.
+        var driving_dirs = restaurantData.getPlaceDrivingDirections(id);
+        var html_str = "";
+        for (var i = 0; i < driving_dirs.length; i++) {
+            html_str += "<p>" + driving_dirs[i] + "</p>";
+        }
+        $("#directions-text2").html(html_str);
+
+        // Get walking directions.
+        var walking_dirs = restaurantData.getPlaceWalkingDirections(id);
+        var html_str = "";
+        for (var i = 0; i < walking_dirs.length; i++) {
+            html_str += "<p>" + walking_dirs[i] + "</p>";
+        }
+        $("#directions-text3").html(html_str);
+
+        var myModal = document.getElementById('myModal');
+        myModal.style.display = "block";
+
+    });
 }
 
 // Insert an entry to a row in the table.
@@ -45,16 +92,19 @@ function insertRow(idx) {
     var walk_time = restaurantData.getPlaceWalkTime(idx);
 
     console.log("name : " + name);
+
+    var rowName = "" + idx;
     // Create the new row
     var newRow = $("<tr>");
+    newRow.attr("id", rowName);
     // Highlight fastest walk in green.
     if (restaurantData.getPlaceIsQuickestWalk(idx) == true) {
-        newRow = $("<tr style=\"color: #40bf40\">");
+        newRow.attr("style", "color: #40bf40");
     }
 
     // Highlight fastest drive in red.
     if (restaurantData.getPlaceIsQuickestDrive(idx) == true) {
-        newRow = $("<tr style=\"color: #ff0000\">");
+        newRow.attr("style", "color: #ff0000");
     }
 
     newRow.append(
